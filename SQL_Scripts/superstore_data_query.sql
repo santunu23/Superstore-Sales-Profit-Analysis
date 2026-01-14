1c11df9dc7f169d30dcf68f1e08bd51e8f385688
Joy <santunu23@gmail.com>
2026-01-14 17:22:54 +0600

    Update superstore_data_query.sql

--- SQL_Scripts/superstore_data_query.sql
+++ SQL_Scripts/superstore_data_query.sql
@@ -1,21 +1,60 @@
-SELECT * FROM `mybigquerypractice-446408.superstore_data.superstore_userdata` order by(uni_id) desc;
+-- Fetch all rows
+SELECT * FROM `database.table_name` order by(uni_id) ASC;
 
+-- check NULL Value
+SELECT *
+FROM `database.table_name`
+WHERE `Ship Mode` IS NULL 
+   OR Sales IS NULL 
+   OR City IS NULL;
+
+-- check blank row
+SELECT COUNT(*) AS total_blank_rows
+FROM `database.table_name`
+WHERE uni_id IS NULL;
+
+-- check duplicate value
+SELECT `uni_id`, COUNT(*)
+FROM `database.table_name`
+GROUP BY `uni_id`
+HAVING COUNT(*) > 1;
+
+--Profitability by Category 
 SELECT 
 Category,
 sum(sales) as Total_sales,
 sum(profit) as Total_profit,
 ROUND((SUM(Profit) / SUM(Sales)) * 100, 2) AS Profit_Margin_Percentage
-FROM `mybigquerypractice-446408.superstore_data.superstore_userdata`
+FROM `database.table_name`
 GROUP BY Category
 ORDER BY Total_Profit DESC;
 
---repeted customers from the region
+-- Regional Performance & Customer Loyalty
 SELECT
 Region,
 COUNT(uni_id) as Total_Order,
 SUM(sales) as Total_sales,
 SUM(profit) as Total_profit,
 ROUND(AVG(Profit), 2) AS Avg_Profit_Per_Order
-FROM `mybigquerypractice-446408.superstore_data.superstore_userdata`
+FROM `database.table_name`
 GROUP BY Region
 ORDER BY Total_Order DESC;
+
+-- The Correlation Between Discount and Profit
+SELECT 
+    Category, 
+    ROUND(AVG(Discount), 4) AS Avg_Discount, 
+    ROUND(SUM(Profit), 2) AS Total_Profit
+FROM `database.table_name`
+GROUP BY Category
+ORDER BY Avg_Discount DESC;
+
+-- Shipping Mode Efficiency
+SELECT 
+    `Ship Mode`, 
+    COUNT(uni_id) AS Total_Orders,
+    SUM(Profit) AS Total_Profit,
+    ROUND(AVG(Profit), 2) AS Avg_Profit_Per_Order
+FROM `database.table_name`
+GROUP BY `Ship Mode`
+ORDER BY Avg_Profit_Per_Order DESC;
